0d0e688a2d2cbeb7249587d270a583fb2ca9902b
sivkovych <sivkovych.p@gmail.com>
2023-03-08 01:46:31 +0200

    test job

---
+++ .github/workflows/check-versions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+base="${{ github.event.pull_request.base.ref }}"
+git fetch origin "${base}":refs/remotes/origin/"${base}"
+
+path=$(git diff --name-only origin/"${base}" | grep pom.xml)
+
+if [ -z "${path}" ]; then
+  echo Cannot find pom.xml in changed files
+  exit 1
+else
+  diff=$(git diff origin/"${base}" "${path}" | sed -e 's| ||g')
+  was=$(echo "${diff}" |
+    grep -Po "(?<=^-<version>)((\d+|.)+?)(?=<\/version>)")
+  become=$(echo "${diff}" |
+    grep -Po "(?<=^-<version>)((\d+|.)+?)(?=<\/version>)")
+  echo "${was}"
+  echo "${become}"
+fi
