d6a246478f5161244c42ba8179e924b30cea02b8
sivkovych <sivkovych.p@gmail.com>
2023-03-08 02:28:14 +0200

    test job

--- .github/workflows/check-versions.sh
+++ .github/workflows/check-versions.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env sh
 
+get_version() {
+  echo "$1" |
+    grep -Po "(?<=^$2<version>)((\d+|.)+?)(?=<\/version>)" |
+    sed -e 's|\.||g'
+}
+get_old_version() {
+  echo get_version "$1" "-"
+}
+get_new_version() {
+  echo get_version "$1" "\+"
+}
+
+file_name="pom.xml"
 base_ref=$1
+
 git fetch -q origin "${base_ref}":refs/remotes/origin/"${base_ref}"
 
-path=$(git diff --name-only origin/"${base_ref}" | grep pom.xml)
+path=$(git diff --name-only origin/"${base_ref}" | grep "${file_name}")
 
 if [ -z "${path}" ]; then
-  echo Cannot find pom.xml in changed files
+  echo "Cannot find [${file_name}] in changed files" >>/dev/stderr
   exit 1
 else
   diff=$(git diff origin/"${base_ref}" "${path}" | sed -e 's| ||g')
-  was=$(echo "${diff}" |
-    grep -Po "(?<=^-<version>)((\d+|.)+?)(?=<\/version>)" |
-    sed -e 's|\.||g')
-  become=$(echo "${diff}" |
-    grep -Po "(?<=^\+<version>)((\d+|.)+?)(?=<\/version>)" |
-    sed -e 's|\.||g')
-  echo "WAS: ${was}"
-  echo "BECOME: ${become}"
-  difference=$((become - was))
-  echo "DIFFERENCE: ${difference}"
+  old_version=$(get_old_version "$diff")
+  new_version=$(get_new_version "$diff")
+  difference=$((new_version - old_version))
   if [ ${difference} -gt 0 ]; then
-    echo "IS OK"
+    echo "Version was incremented by ${difference}"
+    exit 0
   else
-    echo "IS NOT OK"
+    echo "Version was changed but not incremented" >>/dev/stderr
     exit 1
   fi
 fi
